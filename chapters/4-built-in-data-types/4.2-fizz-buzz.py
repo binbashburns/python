3f2e3157a2f94e281ae07fded63d0462b53020a5
matt <pro.mattburns@gmail.com>
2022-02-08 21:33:42 +0000

    Added better comments to 4.2-fizz-buzz.py

--- chapters/4-built-in-data-types/4.2-fizz-buzz.py
+++ chapters/4-built-in-data-types/4.2-fizz-buzz.py
@@ -2,10 +2,10 @@
 
 count = 0                # The count starts with a value of 0
 while(count < 101):      # This loop will keep cycling until "count" is under 100
-    if count % 3 == 0:
-        print ("fizz")
-    elif count % 5 == 0:
-        print ("buzz")
-    else:
-        print(count)     # This will print a number of asterisks equal to the value of "count"
-    count += 1           # This will add one to the total of "count", per cycle
+    if count % 3 == 0:   # If the count is divisible by three...
+        print ("fizz")   # ... print "fizz"
+    elif count % 5 == 0: # Else, if the count is divisible by five...
+        print ("buzz")   # ... print "buzz"
+    else:                # If the count isn't equal to either of those values
+        print(count)     # Just output the number you're on
+    count += 1           # And every time you do this loop, increment the count by 1.
