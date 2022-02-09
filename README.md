6e519969cb066b4a0050921956f3c6fedb06e9b2
matt <pro.mattburns@gmail.com>
2022-02-09 14:08:25 +0000

    Still trucking...

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# python
-Notes from Introduction to Python Development - ACG
+# Python Basics
+## About
+This is just a few basics about Python, mostly for personal reference.</br>
+Nothing impressive, just taking some notes as a result of having gone through Python courses on ACloudGuru.</br>
+If you're interested in checking out the courses for yourself, feel free to check it out, along with ACGs other courses at the link below!</br>
+
+## References used:
+- Introduction to Python Scripting (ACG) with Ermin Kreponic:   https://acloudguru.com/course/introduction-to-python-scripting
+- Introduction to Python Development (ACG) with Keith Thompson: https://acloudguru.com/course/introduction-to-python-development
+- Learn Python at W3 Schools: https://www.w3schools.com/python/
+- Python: https://www.python.org/
--- chapters/5-control-flow/5.1-main.py
+++ chapters/5-control-flow/5.1-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3.7
-### --- Comparisons --- ###
+### --- Conditionals and Comparisons --- ###
 print(1 < 2) # This returns a boolean of whether this is true or not. This would evaluate to True
 print(1 > 2) # This would return a "False"
 
@@ -18,3 +18,29 @@ print("Is the left operand smaller than the right operand: <")
 print("Is the left operand bigger or equal to the right operand: >=")
 print("Is the left operand smaller or equal to the right operand: <=")
 
+# In
+print(2 in [1,2,3]) # This will evaluate to True, as there is a 2 in the list
+print(4 in [1,2,3]) # This will evaluate to False, as there is no 4 in the list
+print(4 not in [1,2,3]) # This will evaluate to True, as there is no 4 in the list
+
+# If / Elif / Else
+test = 1                # Establishes "test" with a value of 1
+if test > 0:            # Start your statement with a condition. In this case, we're checking if "test" is greater than zero
+    print("Positive")   # If "test" is greater than zero, it will print this
+elif test < 0:          # You can have as many "elifs" in between as you need. This is checking if "test" is lesser than zero
+    print("Negative")   # If "test" is lesser than zero, it will print this
+else:                   # You have to end your conditional with an "else:" to give one last option to select.
+    print("ZERO")       # If test is not greater or lesser than zero, it will print "ZERO"
+
+# Now let's try another one, but nested
+test = 1
+if test > 0:                                # First layer of nesting. Evaluates if the number is > 0
+    if test > 10 and test < 15:             # Second layer. Evaluates if the number, that we've identified > 0, is over 15
+        print("Positive in range: 10 - 15") # If the result is over zero but under 15, it prints this
+    else:                                   # If the result is over zero and over 15, it prints this
+        print("Positive greater than 15")
+elif test < 0:
+    print("Negative")
+else:
+    print("ZERO")
+    
