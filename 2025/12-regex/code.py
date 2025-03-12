eca296661481b57b50747638a77ce300d730ec2e
Matt Burns <pro.mattburns@gmail.com>
2025-03-12 12:46:04 -0400

    troubleshoot script

--- 2025/12-regex/code.py
+++ 2025/12-regex/code.py
@@ -5,29 +5,52 @@
 
 # ########################################
 
-# From the given string, use regex to find and print all emails with a yahoo domain and a three letter first name.
+# # From the given string, use regex to find and print all emails with a yahoo domain and a three letter first name.
 
-import re 
-strng = '''Bill has an email address Bill@email.com
-           Suzanne has an email address Suzanne@other.org
-           Joe has an email address Joe@yahoo.com
-           Jill has an email address Jill@yahoo.com
-           Jim has an email address Jim@yahoo.com
-           Dre has an email address Dre@google.com
-           Han has an email address Han@yahoo.com
-           Jen has an email address Jennifer@yahoo.com
+# import re 
+# strng = '''Bill has an email address Bill@email.com
+#            Suzanne has an email address Suzanne@other.org
+#            Joe has an email address Joe@yahoo.com
+#            Jill has an email address Jill@yahoo.com
+#            Jim has an email address Jim@yahoo.com
+#            Dre has an email address Dre@google.com
+#            Han has an email address Han@yahoo.com
+#            Jen has an email address Jennifer@yahoo.com
+#            '''
+
+# pattern = r'\b[A-Za-z]{3}@yahoo\.com\b'
+
+# matches = re.findall(pattern, strng)
+
+# lst = []
+
+# for match in matches:
+#     lst.append(match)
+
+# print(lst)
+
+########################################
+import re
+
+strng = '''(212)555-1234
+           (123)45-2345
+           415-555-6789
+           123-123-123
+           (305)555-4321
+           818-555-9876
+           (312)555-8765
            '''
 
-pattern = r'\b[A-Za-z]{3}@yahoo\.com\b'
+pattern = r'+\d\d\d+\d\d\d+\d\d\d\d+'
 
 matches = re.findall(pattern, strng)
 
+lst = []
+
 for match in matches:
-    print(match)
+    lst.append(match)
 
-########################################
-
-
+print(lst)
 
 ########################################
 
